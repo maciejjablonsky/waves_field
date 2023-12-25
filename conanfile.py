f450c49af7cedfd8ac27c8d341402322b211c9b3
maciejjablonsky <maciekjablonsky@gmail.com>
2023-12-26 00:24:12 +0100

    Added cmake and ninja for VS usage

--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,6 @@
 from conan import ConanFile
 from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
-
+import os
 
 class waves_field(ConanFile):
     name = "waves_field"
@@ -18,6 +18,9 @@ class waves_field(ConanFile):
     def layout(self):
         cmake_layout(self)
 
+    def build_requirements(self):
+        self.tool_requires("ninja/1.11.1")
+
     def requirements(self):
         self.requires("fmt/10.1.1")
         self.requires("glfw/3.3.8")
@@ -30,6 +33,8 @@ class waves_field(ConanFile):
         tc = CMakeToolchain(self)
         tc.generate()
 
+        self._update_cmake_presets()
+
     def build(self):
         cmake = CMake(self)
         cmake.configure()
@@ -38,3 +43,50 @@ class waves_field(ConanFile):
     def package(self):
         cmake = CMake(self)
         cmake.install()
+
+    def _update_cmake_presets(self):
+        cmake_presets_path = os.path.join(
+            self.build_folder, "generators", "CMakePresets.json"
+        )
+        assert os.path.exists(cmake_presets_path)
+
+        self._add_cmake_executable_to_cmake_presets(cmake_presets_path)
+        self._add_ninja_executable_to_cmake_presets(cmake_presets_path)
+
+    def _add_cmake_executable_to_cmake_presets(self, cmake_presets_path):
+        import shutil
+        cmake_presets = self._load_json_from_file(cmake_presets_path)
+
+        cmake_path = os.path.dirname(shutil.which("cmake"))
+        self.output.info(
+            f'Setting "cmakeExecutable" in {cmake_presets_path} to "{cmake_path}"'
+        )
+        for configuration_preset in cmake_presets["configurePresets"]:
+            configuration_preset["cmakeExecutable"] = os.path.join(
+                cmake_path, "cmake.exe"
+            )
+        self._save_json_to_file(cmake_presets_path, cmake_presets)
+
+    def _add_ninja_executable_to_cmake_presets(self, cmake_presets_path):
+        cmake_presets = self._load_json_from_file(cmake_presets_path)
+        ninja_path = self.dependencies.build["ninja"].cpp_info.bindirs[0]
+        self.output.info(
+            f'Setting "ninjaExecutable" in {cmake_presets_path} to "{ninja_path}"'
+        )
+        for configuration_preset in cmake_presets["configurePresets"]:
+            configuration_preset["ninjaExecutable"] = os.path.join(
+                ninja_path, "ninja.exe"
+            )
+        self._save_json_to_file(cmake_presets_path, cmake_presets)
+
+    def _load_json_from_file(self, path):
+        import json
+
+        with open(path, "r") as file:
+            return json.load(file)
+
+    def _save_json_to_file(self, path, content):
+        import json
+
+        with open(path, "w") as file:
+            file.write(json.dumps(content, indent=4))
