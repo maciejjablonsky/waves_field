685241318f8cb391b481bbfa1bd27ff200dc4be7
maciejjablonsky <maciekjablonsky@gmail.com>
2024-03-07 02:17:27 +0100

    Added clang profile

---
+++ setup_clang_debug.ps1
@@ -0,0 +1,19 @@
+param(
+    [switch]$installOnly
+)
+
+Write-Host "Running Conan install..."
+conan install . -pr:h dev-clang -b missing -s build_type=Debug
+
+if ($installOnly) {
+    exit 0
+}
+
+# Check if the previous command was successful
+if ($LASTEXITCODE -ne 0) {
+    Write-Error "Conan install failed."
+    exit $LASTEXITCODE
+}
+
+Write-Host "Executing the bat script and opening Visual Studio..."
+cmd /c "build\Debug\generators\conanbuild.bat && devenv ."
